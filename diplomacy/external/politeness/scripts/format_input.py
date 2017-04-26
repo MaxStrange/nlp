dbb54cd115abf4065af4b5d4d67ed85e9b2126ec
MaxStrange <max.strange@synapse.com>
2017-04-25 17:45:06 -0700

    Added politeness external library.

---
+++ diplomacy/external/politeness/scripts/format_input.py
@@ -0,0 +1,67 @@
+import json
+import os
+import re
+import requests
+import sys
+import traceback
+
+from json import JSONDecodeError
+from requests.exceptions import RequestException
+from nltk.tokenize import sent_tokenize
+from pycorenlp import StanfordCoreNLP
+
+nlpserver = StanfordCoreNLP("http://localhost:9000")
+
+def clean_depparse(dep):
+    """
+    Given a dependency dictionary, return a formatted string representation.
+    """
+    return str(dep['dep'] + "(" + dep['governorGloss'].lower() + "-" +
+               str(dep['governor']) + ", " + dep['dependentGloss'] + "-" +
+               str(dep['dependent']) + ")")
+
+def clean_treeparse(tree):
+    cleaned_tree = re.sub(r' {2,}', ' ', tree)
+    cleaned_tree = re.sub(r'\n', '', cleaned_tree)
+    cleaned_tree = re.sub(r'\([^\s]*\s', '', cleaned_tree)
+    cleaned_tree = re.sub(r'\)', '', cleaned_tree)
+    cleaned_tree = re.sub(r'-LRB-', '(', cleaned_tree)
+    cleaned_tree = re.sub(r'-RRB-', ')', cleaned_tree)
+
+    return cleaned_tree
+
+def get_sentences(doc_text):
+    temp = doc_text.strip()#.split('\n')
+    sents = []
+    for s in temp:
+        sents += sent_tokenize(s.strip())
+
+    return sents
+
+def get_parses(sent):
+    parse = {'deps': [], 'sent': ""}
+    response = nlpserver.annotate(sent, properties={'annotators': 'tokenize,ssplit,pos,parse,depparse', "outputFormat": "json"})
+    for sentence in response['sentences']:
+        parse['deps'] = sentence['enhancedPlusPlusDependencies']
+        parse['sent'] = sent
+
+    return parse
+
+def format_doc(doc_text):
+    print("Getting the sentences...")
+    sents = get_sentences(doc_text)
+    print("Parsing the sentences...")
+    raw_parses = []
+    for sent in sents:
+        raw_parses.append(get_parses(sent))
+    print("Formatting the raw parses...")
+    results = []
+    for raw in raw_parses:
+        result = {'parses': [], 'sentences': []}
+        for dep in raw['deps']:
+            result['parses'].append(clean_depparse(dep))
+        result['sentences'].append(clean_treeparse(raw['sent']))
+
+        results.append(result)
+
+    return results
